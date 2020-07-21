5b793e02a60d48d7ea33df79839ab64ef88b2e2c
Michael "Z" Goddard <mzgoddard@gmail.com>
2020-07-21 11:38:15 -0400

    typescript: add types to public interfaces
    - type param validators and by extensions the params to Query
    - type bound methods with callbackToPromise with overloaded interfaces

--- src/promise.ts
+++ src/promise.ts
@@ -1,5 +1,13 @@
 /* global Promise */
-var polyfill = require('es6-promise');
+import polyfill from 'es6-promise';
 
 // istanbul ignore next
-module.exports = typeof Promise === 'undefined' ? polyfill.Promise : Promise;
+export = typeof Promise === 'undefined'
+    ? // Cast the polyfill version to **any** to lose the implementation's
+      // details, then to PromiseConstructor so it matches the native
+      // implementation. Otherwise this file exports a type union of
+      // polyfill.Promise and PromiseContructor. Since those are different it
+      // makes it hard or not possible to fulfill the right types when creating
+      // new Promises.
+      ((polyfill.Promise as any) as PromiseConstructor)
+    : Promise;
