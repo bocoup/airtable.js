287b0edad1b7cefafb82dafe66d0199927ef1a2b
Michael "Z" Goddard <mzgoddard@gmail.com>
2020-09-30 17:26:47 -0400

    fixup! Update abort-controller.ts

--- src/abort-controller.ts
+++ src/abort-controller.ts
@@ -1,9 +1,9 @@
 // istanbul ignore file
 let AbortController: new () => AbortController;
 
-if (typeof window === 'undefined' || typeof window.Request === 'undefined') {
+if (typeof window === 'undefined' && typeof Request === 'undefined') {
     AbortController = require('abort-controller');
-} else if ('signal' in new window.Request('')) {
+} else if ('signal' in new Request('') && typeof window.AbortController !== 'undefined') {
     AbortController = window.AbortController;
 } else {
     const polyfill = require('abortcontroller-polyfill/dist/cjs-ponyfill');
